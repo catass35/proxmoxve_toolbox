7bfd0446418042803bc8c6c38ebab5ace6be013b
catass35 <175985261+catass35@users.noreply.github.com>
2025-03-21 21:34:55 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,7 @@ provider "proxmox" {
   endpoint  = "https://10.0.0.2:8006/"
   api_token = "terraform-prov@pve!provider=${local.token}"
 }
+
+data "proxmox_virtual_environment_user" "operations_user" {
+  user_id = "terraform-prov@pve"
+}

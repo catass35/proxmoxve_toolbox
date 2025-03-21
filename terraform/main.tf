3f274c962e26d907d8909ab172a7f0ed28b8180b
catass35 <175985261+catass35@users.noreply.github.com>
2025-03-21 21:30:43 +0100

    Update main.tf
    add token

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.73.2"
+    }
+  }
+}
 
+locals {
+  token = var.TF_VAR_api_token
+}
+
+provider "proxmox" {
+  endpoint  = "https://10.0.0.2:8006/"
+  api_token = "terraform-prov@pve!provider=${local.token}"
+}
